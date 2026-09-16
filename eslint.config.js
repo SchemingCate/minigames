9f67aead222c6f147bd9f7ea7b0f781ec3974a9f
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2026-09-17 00:59:28 +0300

    chore: configure prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import unicorn from 'eslint-plugin-unicorn';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig(
   {
@@ -30,5 +31,7 @@ export default defineConfig(
     linterOptions: {
       noInlineConfig: true,
     },
-  }
+  },
+
+  eslintConfigPrettier,
 )
