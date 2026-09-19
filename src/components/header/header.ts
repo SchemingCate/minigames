2ef2176d44cec6b7e373a5664c1de78dd89737a2
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2026-09-20 00:14:08 +0300

    feat: style header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,10 +1,16 @@
+import './header.scss';
 import { logo } from '../logo/logo';
 import { button } from '../button/button';
 
 export const header: () => Node = () => {
   const headerElement = document.createElement('header');
+  headerElement.classList = 'header';
+  const headerLinks = document.createElement('div');
+  headerLinks.classList = 'header_links';
   const nav = document.createElement('nav');
   const navList = document.createElement('ul');
+  navList.classList = 'nav_list';
+  headerLinks.append(nav, button('Log in'), button('Sign up'));
   nav.append(navList);
 
   for (const element of ['home', 'library', 'tournaments', 'community']) {
@@ -13,6 +19,6 @@ export const header: () => Node = () => {
     navList.append(listItem);
   }
 
-  headerElement.append(logo(), nav, button('Log in'), button('Sign up'));
+  headerElement.append(logo(), headerLinks);
   return headerElement;
 };
--- src/components/logo/logo.ts
+++ src/components/logo/logo.ts
@@ -1,7 +1,15 @@
+import './logo.scss';
+import logoSvg from '../../assets/logos/logo.svg';
+
 export const logo: () => Node = () => {
   const logoElement = document.createElement('div');
+  logoElement.classList = 'logo'
+  const logoImg = document.createElement('img');
+  logoImg.src = logoSvg;
+  logoImg.alt = 'MiniGames logo'
   const title = document.createElement('h1');
   title.textContent = 'MiniGames';
-  logoElement.append(title);
+  title.classList = 'logo_title';
+  logoElement.append(logoImg, title);
   return logoElement;
 };
