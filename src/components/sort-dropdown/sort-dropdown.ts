3abdd44249869502209695950f6bfa5b0665e548
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2026-09-27 00:53:06 +0300

    feat: implement sort element

--- src/components/sort-dropdown/sort-dropdown.ts
+++ src/components/sort-dropdown/sort-dropdown.ts
@@ -1,7 +1,88 @@
 import './sort-dropdown.scss';
 import { createHTMLElement } from '../../helpers/dom';
 
+const sortOptions = [
+  {
+    slug: 'rating-highest',
+    label: 'Rating ↑',
+    isActive: true,
+  },
+  {
+    slug: 'rating-lowest',
+    label: 'Rating ↓',
+    isActive: false,
+  },
+  {
+    slug: 'rating-a-z',
+    label: 'Name A→Z',
+    isActive: false,
+  },
+  {
+    slug: 'rating-z-a',
+    label: 'Name Z→A',
+    isActive: false,
+  },
+];
+
 export const sortDropdown = (): HTMLElement => {
-  const sort = createHTMLElement({ tag: 'div', textContent: 'Sort by' });
+  const sort = createHTMLElement({ tag: 'div', classList: 'sort' });
+  const sortButton = createHTMLElement({
+    tag: 'button',
+    classList: 'sort_button',
+    textContent: 'Sort by: Rating ↑',
+    attributes: [
+      ['id', 'sort-button'],
+      ['type', 'button'],
+      ['aria-haspopup', 'listbox'],
+      ['aria-expanded', 'false'],
+    ],
+  });
+
+  const sortList = createHTMLElement({
+    tag: 'ul',
+    classList: 'sort_list',
+    attributes: [
+      ['role', 'listbox'],
+      ['aria-labelledby', 'sort-button'],
+      ['hidden', ''],
+    ],
+  });
+
+  let selectedId = 'rating-highest';
+  for (const { slug, label, isActive } of sortOptions) {
+    const optionElement = createHTMLElement({
+      tag: 'li',
+      textContent: label,
+      classList: 'sort_list_item',
+      attributes: [
+        ['id', slug],
+        ['role', 'option'],
+        ['aria-selected', isActive.toString()],
+      ],
+    });
+    sortList.append(optionElement);
+  }
+
+  sort.append(sortButton, sortList);
+
+  sort.addEventListener('click', (event) => {
+    const target =
+      (event.target as HTMLElement).closest('.sort_button') ||
+      (event.target as HTMLElement).closest('.sort_list_item');
+    if (!target) return;
+    console.log('click');
+    const isAriaExpandedNew =
+      sortButton.getAttribute('aria-expanded') !== 'true';
+    sortButton.setAttribute('aria-expanded', isAriaExpandedNew.toString());
+    sortList.toggleAttribute('hidden');
+
+    if (!target.classList.contains('sort_list_item')) return;
+    const previous = sort.querySelector(`#${selectedId}`);
+    previous?.setAttribute('aria-selected', 'false');
+    selectedId = target.id;
+    target.setAttribute('aria-selected', 'true');
+    sortButton.textContent = `Sort by: ${target.textContent}`;
+  });
+
   return sort;
 };
